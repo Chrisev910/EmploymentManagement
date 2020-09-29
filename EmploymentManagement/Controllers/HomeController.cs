46b6eca2891b48cfb4482982e4f600463b5543ea
Chris Hammond <chammond@qeliz.ac.uk>
2020-09-29 16:22:01 +0100

    Up to video https://www.youtube.com/watch?v=Lu24lZsUreg&list=PL6n9fhu94yhVkdrusLaQsfERmL_Jh4XmU&index=26

--- EmploymentManagement/Controllers/HomeController.cs
+++ EmploymentManagement/Controllers/HomeController.cs
@@ -2,22 +2,31 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmploymentManagement.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmploymentManagement.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IEmployeeRepository _employeeRepository;
 
-        public JsonResult Index()
+        public HomeController(IEmployeeRepository employeeRepository)
         {
-            JsonResult result;
+            _employeeRepository = employeeRepository;
+        }
 
-            result = Json(new { id = 1, name = "Chris" });
+        public string Index()
+        {
+           return _employeeRepository.GetEmployee(1).Name;
+        }
 
-            return result;
-                
-                
+        public ViewResult Details()
+        {
+            Employee model = _employeeRepository.GetEmployee(1);
+            ViewBag.Employee = model;
+            ViewBag.PageTitle = "Employee Details";
+            return View(model);
         }
     }
 }
--- EmploymentManagement/Startup.cs
+++ EmploymentManagement/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EmploymentManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -18,7 +19,9 @@ namespace EmploymentManagement
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(option => option.EnableEndpointRouting = false);
+            services.AddMvc(option => option.EnableEndpointRouting = false).AddXmlSerializerFormatters();
+            services.AddSingleton<IEmployeeRepository, MockEmployeeRepository>();
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
